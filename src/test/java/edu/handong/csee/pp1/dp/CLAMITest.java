65aa8ac8f7b87dbae2d77f8301d553e0809c5bc5
hasung <hasung@172.17.216.49>
2018-11-03 16:36:29 +0900

    #3

--- src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
+++ src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
@@ -60,11 +60,21 @@ public class CLAMITest {
 		// TASK3: Conduct defect prediction by using CLAMI models
 		// complete this test case for the generated arff files from Task 2
 		
-		//String[] args = {"-f","data/[your_generated_metric_data].arff","-l","label","-p", "buggy", "-m"};
+		String[] args = {"-f","data/javaplot.arff","-l","label","-p", "buggy", "-m"};
 		
-		//CLAMI.main(args);
+		CLAMI.main(args);
 	}
 	
+	@Test
+	public void testCLAMIWithYourSelectedProject2() {
+		
+		// TASK3: Conduct defect prediction by using CLAMI models
+		// complete this test case for the generated arff files from Task 2
+		
+		String[] args = {"-f","data/TooLargeTooSmall","-l","label","-p", "buggy", "-m"};
+		
+		CLAMI.main(args);
+	}
 	@Test
 	public void testCLAMIWithApacheProjectToFindBestCutoffForPrecision() {
 		
